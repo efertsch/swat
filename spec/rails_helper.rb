58f1ff86ceb0652a5cda992bcf5b097ce5e02926
[01;31m[K"name"[m[K: "Ethan Fertsch <efertsch@gmail.com>
2019-09-21 09:19:50 -0400

    Requires files and extends database cleaner config

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -5,6 +5,9 @@ require File.expand_path('../../config/environment', __FILE__)
 # Prevent database truncation if the environment is production
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'rspec/rails'
+require 'database_cleaner'
+require 'capybara/rspec'
+require 'support/factory_bot'
 # Add additional requires below this line. Rails is not loaded until this point!
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
@@ -37,7 +40,7 @@ RSpec.configure do |config|
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
   # instead of true.
-  config.use_transactional_fixtures = true
+  config.use_transactional_fixtures = false
 
   # RSpec Rails can automatically mix in different behaviours to your tests
   # based on their file location, for example enabling you to call `get` and
@@ -71,14 +74,33 @@ RSpec.configure do |config|
     end
   end
 
-  config.before(:suite) do
-    DatabaseCleaner.strategy = :transaction
-    DatabaseCleaner.clean_with(:truncation)
-  end
 
-  config.around(:each) do |example|
-    DatabaseCleaner.cleaning do
-      example.run
-    end
-  end
+  # Database Cleaner Config
+  config.before(:suite) do
+     DatabaseCleaner.clean_with(:truncation)
+   end
+
+   config.before(:each) do
+     DatabaseCleaner.strategy = :transaction
+   end
+
+   config.before(:each, :js => true) do
+     DatabaseCleaner.strategy = :truncation
+   end
+
+   config.before(:each) do
+     DatabaseCleaner.start
+   end
+
+   config.after(:each) do
+     DatabaseCleaner.clean
+   end
+
+   config.before(:all) do
+     DatabaseCleaner.start
+   end
+
+   config.after(:all) do
+     DatabaseCleaner.clean
+   end
 end
